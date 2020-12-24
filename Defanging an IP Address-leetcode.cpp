92f15d322f2f330f6dd0aa8b3ea7b19e5d96c1f7
Pradeep <56883812+pradeepelamurugu@users.noreply.github.com>
2020-12-24 16:29:49 +0530

    Create Defanging an IP Address-leetcode.cpp

---
+++ Defanging an IP Address-leetcode.cpp
@@ -0,0 +1,20 @@
+//link: https://leetcode.com/problems/defanging-an-ip-address/
+
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string ans;
+        for(char ch: address){
+            if(ch=='.'){
+                ans.push_back('[');
+                ans.push_back('.');
+                ans.push_back(']');
+                
+            }
+            else{
+                ans.push_back(ch);
+            }
+        }
+        return ans;
+    }
+};
