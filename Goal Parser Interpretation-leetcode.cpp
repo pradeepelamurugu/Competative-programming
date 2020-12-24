bc2538a5f374e0b5f24940cb7c0f22c8d02b893f
Pradeep <56883812+pradeepelamurugu@users.noreply.github.com>
2020-12-24 16:33:50 +0530

    Create Goal Parser Interpretation-leetcode.cpp

---
+++ Goal Parser Interpretation-leetcode.cpp
@@ -0,0 +1,21 @@
+//link: https://leetcode.com/problems/goal-parser-interpretation/
+
+class Solution {
+public:
+    string interpret(string command) {
+        string ans;
+        for(int i=0;i<command.length();i++){
+            if(command[i]=='G') ans.push_back('G');
+            else if(command[i]=='(' && command[i+1]==')'){
+                ans.push_back('o');
+                i++;
+            }
+            else if(command[i]=='(' && command[i+1]=='a'){
+                ans.push_back('a');
+                ans.push_back('l');
+                i+=3;
+            }
+        }
+        return ans;
+    }
+};
